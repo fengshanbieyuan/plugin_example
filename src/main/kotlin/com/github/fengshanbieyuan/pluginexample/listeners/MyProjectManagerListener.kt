2ace824b3df4c0578146ece86e8b0e7b723cd466
GitHub Action <action@github.com>
2020-12-16 14:52:25 +0000

    Template cleanup

--- src/main/kotlin/com/github/fengshanbieyuan/pluginexample/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/fengshanbieyuan/pluginexample/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.fengshanbieyuan.pluginexample.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.fengshanbieyuan.pluginexample.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/fengshanbieyuan/pluginexample/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.fengshanbieyuan.pluginexample.services
+
+import com.github.fengshanbieyuan.pluginexample.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
